b8d09dbdf2463ff1c76e9d96dc8f1bd9fdded170
Wesley <wesley_snaine@hotmail.com>
2022-01-03 23:16:16 -0300

    update functions to display a book on the page when created, add some style

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 
-function Book (title, author, pages) {
+function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
 
 const titleInput = document.querySelector('#title');
@@ -11,10 +12,35 @@ const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
 const submitBtn = document.querySelector('.submit');
+const bookDisplayer = document.querySelector('.displayBooks')
 
 function addBookToLibrary () {
-    const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
+    const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
     myLibrary.push(newBook);
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readInput.checked = false;
+    displayTheBook(newBook);
 }
 
 submitBtn.addEventListener('click', addBookToLibrary);
+
+function displayTheBook (obj) {
+    const bookBox = document.createElement('div');
+
+    let textContainer = document.createElement('p');
+    textContainer.textContent = `Title: ${obj.title}`;
+    bookBox.appendChild(textContainer);
+
+    textContainer = document.createElement('p');
+    textContainer.textContent = `Author: ${obj.author}`;
+    bookBox.appendChild(textContainer);
+
+    textContainer = document.createElement('p');
+    textContainer.textContent = `Pages: ${obj.pages}`;
+    bookBox.appendChild(textContainer);
+
+    bookBox.classList.add('bookBox');
+    bookDisplayer.appendChild(bookBox);
+}
