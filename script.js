70e9dfe5a5dd51f1c1d9f76d81784efc39ae9ea5
wesley <wesley_snaine@hotmail.com>
2022-07-11 17:36:29 -0300

    fix layout issues

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let myLibrary = [
-    {title: "Book", author: "Author", pages: 342, read: false},
-    {title: "Paper", author: "Nothing", pages: 46, read: false},
-    {title: "Nothing", author: "Hello", pages: 123, read: true},
-    {title: "Random", author: "Name", pages: 856, read: false}
+    {title: "Book", author: "Author", pages: 342, read: false, description: "aaaa"},
+    {title: "Paper", author: "Nothing", pages: 46, read: false, description: "stopped on page 89"},
+    {title: "Nothing", author: "Hello", pages: 123, read: true, description: "Null"},
+    {title: "Random", author: "Name", pages: 856, read: false, description: "a book"}
 ]; //where book objects will be stored
 
 // Code to control the hidden book form.
@@ -22,11 +22,12 @@ document.querySelector('.close').addEventListener('click', closeBookForm);
 
 
 // Book object constructor, the main objective of this project.
-function Book (title, author, pages, read) {
+function Book (title, author, pages, read, description) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.description = description;
 }
 /*** Using prototype will avoid javascript from copying the
 same function for each object, reducing memory usage ***/
@@ -44,8 +45,14 @@ function addBookToLibrary () {
         document.querySelector('#author').value,
         document.querySelector('#pages').value,
         document.querySelector('#read').checked,
+        document.querySelector('#description').value
     ));
     closeBookForm();
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (i >= document.querySelectorAll('.card').length) {
+            createBookCard(myLibrary[i]);
+        }
+    }
 }
 
 function createBookCard (book) {
@@ -54,18 +61,30 @@ function createBookCard (book) {
     let title = document.createElement('h3');
     title.textContent = book.title;
     let author = document.createElement('p');
-    author.textContent = book.author;
+    author.textContent = '-' + book.author;
     let pages = document.createElement('p');
-    pages.textContent = book.pages;
-    let read = document.createElement('p');
-    read.textContent = book.read;
+    pages.textContent = book.pages + ' pages';
+    let read = document.createElement('input');
+    let label = document.createElement('label');
+    label.for = 'read';
+    label.textContent = 'Read ';
+    read.type = 'checkbox';
+    read.id = 'read';
+    read.checked = book.read;
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
+    card.appendChild(label);
     card.appendChild(read);
+    let description = document.createElement('h4');
+    description.textContent = 'Description:';
+    card.appendChild(description);
+    description = document.createElement('p');
+    description.textContent = book.description;
+    card.appendChild(description);
     document.querySelector('.books').appendChild(card);
 }
 
-for (let i = 0; i < myLibrary.length-1; i++) {
+for (let i = 0; i < myLibrary.length; i++) {
     createBookCard(myLibrary[i]);
 }
